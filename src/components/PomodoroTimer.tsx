1db13005e5c6f27720a3aed1412ac85d652683a1
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-13 18:26:43 -0300

    adicionando funcionalidade de start, pause e play do pomodoro

--- src/components/PomodoroTimer.tsx
+++ src/components/PomodoroTimer.tsx
@@ -1,27 +1,54 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { useInterval } from '../hooks/useInterval';
 import { Button } from './Button';
 import { Timer } from './Timer';
 
 type PomodoroProps = {
-  defaultPomodoroTimer: number;
+  pomodoroTime: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 };
 
-export function PomodoroTimer({
-  defaultPomodoroTimer,
-}: PomodoroProps): JSX.Element {
-  const [mainTimer, setMainTimer] = useState(defaultPomodoroTimer);
+export function PomodoroTimer({ pomodoroTime }: PomodoroProps): JSX.Element {
+  const [mainTimer, setMainTimer] = useState(pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
 
-  useInterval(() => {
-    setMainTimer(mainTimer - 1);
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
+
+  useInterval(
+    () => {
+      setMainTimer(mainTimer - 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  };
 
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTimer={mainTimer} />
-      <Button text="teste" onClick={() => console.log(1)} />
+
+      <div className="controls">
+        <Button text="Work" onClick={configWork} />
+        <Button
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        />
+      </div>
+
+      <div className="details">
+        <p>asf</p>
+      </div>
     </div>
   );
 }
